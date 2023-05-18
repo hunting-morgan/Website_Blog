ace4a40b571e04c6d2f9984a3dbcbe94cb29b986
hunting-morgan <50188727+hunting-morgan@users.noreply.github.com>
2023-05-18 15:42:51 -0700

    Fetching_News_hackernews

---
+++ script.js
@@ -0,0 +1,41 @@
+﻿// Fetch cybersecurity news from an API or RSS feed
+function fetchCybersecurityNews() {
+    // Replace the API_URL with the actual API endpoint or RSS feed URL
+    return fetch(https://thehackernews.com/search/label/API%20Security)
+        .then(response => response.json())
+        .then(data => data.articles);
+}
+
+// Create a new post element
+function createPost(title, content) {
+    const article = document.createElement('article');
+    const postTitle = document.createElement('h3');
+    const postContent = document.createElement('p');
+
+    postTitle.textContent = title;
+    postContent.textContent = content;
+
+    article.appendChild(postTitle);
+    article.appendChild(postContent);
+
+    return article;
+}
+
+// Display cybersecurity news on the page
+function displayNews() {
+    const newsContainer = document.getElementById('newsContainer');
+
+    fetchCybersecurityNews()
+        .then(articles => {
+            articles.forEach(article => {
+                const post = createPost(article.title, article.content);
+                newsContainer.appendChild(post);
+            });
+        })
+        .catch(error => {
+            console.error('Error fetching cybersecurity news:', error);
+        });
+}
+
+// Call the displayNews function when the page loads
+window.addEventListener('load', displayNews);
